5bf2f40fbb5af4b7b5667e5f7f3c1adc691dfddc
Priyanshu Patel <patelanshu702@gmail.com>
2025-08-30 16:59:00 +0530

    Time: 2 ms (92.71%), Space: 22.9 MB (16.93%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        int ans = 0;
+        // int ans = 0;
+        // int n = nums.size();
+        // for(int i = 0; i<n; i++){
+        //     for(int j = i+1; j<n; j++){
+        //         ans += __builtin_popcount(nums[i]^nums[j]);
+        //     }
+        // }
+        // return ans;
+
         int n = nums.size();
-        for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                ans += __builtin_popcount(nums[i]^nums[j]);
+        int total = 0;
+        for(int bit = 0; bit<32; bit++){
+            int countone = 0;
+            for(auto it : nums){
+                countone += (it>>bit) &1;
             }
+            int countzero = n-countone;
+            total += countzero * countone;
         }
-    return ans;
+        return total;
     }
 };
