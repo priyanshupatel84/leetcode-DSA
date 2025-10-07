9f2f243453142da573925ef89eb001c5385bfc6a
Priyanshu Patel <patelanshu702@gmail.com>
2025-10-07 20:11:24 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.44%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int cnt = 0;    
         int n = s.length();
 
-
+        int cn = 0;
         for(int i = 0; i<n; i++){
-            // for odd number of element in palindroom 
-            int l = i, r = i;
-            while(l>= 0 && r<n && s[l] == s[r]){
-                cnt++;
+            int l = i;
+            int r = i;
+            while( l>= 0 && r<n && s[l] == s[r]){
+                cn++;
                 l--;
                 r++;
             }
 
-            l = i, r = i+1;
-            while(l >= 0 && r<n && s[l] == s[r]){
-                cnt++;
+            l = i;
+            r = i+1;
+            while( l>= 0 && r<n && s[l] == s[r]){
+                cn++;
                 l--;
                 r++;
             }
         }
 
-        return cnt;
+        return cn;
     }
 };
