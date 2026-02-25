2c28fcb7083efb198b9dc485b55b9f7ccf669bec
Priyanshu Patel <patelanshu702@gmail.com>
2026-02-25 21:24:15 +0530

    Time: 0 ms (100%), Space: 14.2 MB (58.88%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        int n = arr.size();
-        vector<pair<int, int>> vis;
-        for(auto it : arr){
-            int x = __builtin_popcount(it);
-            vis.push_back({it, x});
-        }
-        sort(vis.begin(), vis.end(), [](const auto&a, const auto&b){
-            if(a.second == b.second){
-                return a.first < b.first;
-            }
-            return a.second < b.second;
+        // vector<pair<int, int>> vis;
+        // for(auto it : arr){
+        //     int x = __builtin_popcount(it);
+        //     vis.push_back({it, x});
+        // }
+        // sort(vis.begin(), vis.end(), [](const auto&a, const auto&b){
+        //     if(a.second == b.second){
+        //         return a.first < b.first;
+        //     }
+        //     return a.second < b.second;
+        // });
+        // vector<int> ans;
+        // for(auto it: vis){
+        //     ans.push_back(it.first);
+        // }
+        // return ans;
+
+
+        // better code 
+         sort(arr.begin(), arr.end(), [](int a, int b) {
+            int ca = __builtin_popcount(a);
+            int cb = __builtin_popcount(b);
+            return ca != cb ? ca < cb : a < b;
         });
-        vector<int> ans;
-        for(auto it: vis){
-            ans.push_back(it.first);
-        }
-        return ans;
+
+        return arr;
     }
 };
